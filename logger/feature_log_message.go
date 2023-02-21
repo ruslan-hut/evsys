926776ba9938ad472553a2c8d5e282692ea63571
programador <rkhut@h2plt.energy>
2023-02-21 12:26:31 +0100

    fix: bson fields annotations

--- logger/feature_log_message.go
+++ logger/feature_log_message.go
@@ -5,7 +5,7 @@ const FeatureLogMessageType = "featureLogMessage"
 type FeatureLogMessage struct {
 	Time          string `json:"time" bson:"time"`
 	Feature       string `json:"feature" bson:"feature"`
-	ChargePointId string `json:"id" bson:"chargePointId"`
+	ChargePointId string `json:"id" bson:"charge_point_id"`
 	Text          string `json:"text" bson:"text"`
 }
 
