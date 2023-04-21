2b3ee0884c873343ae5b80eb1152f1a3ee241049
ruslanhut <dev@programmer.com.ua>
2023-04-22 00:00:54 +0200

    fix: bson annotation

--- models/charge_point_status.go
+++ models/charge_point_status.go
@@ -1,7 +1,7 @@
 package models
 
 type ChargePointStatus struct {
-	ChargePointID string            `json:"charge_point_id" bson:"charge_point_id"`
+	ChargePointID string            `json:"charge_point_id" bson:"_id"`
 	ConnectorID   int               `json:"connector_id" bson:"connector_id"`
 	Status        string            `json:"status" bson:"status"`
 	Time          string            `json:"time" bson:"time"`
