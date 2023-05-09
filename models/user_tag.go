4faee1fd831ab2c093ef9688bc708ff98c0d6c46
programador <rkhut@h2plt.energy>
2023-05-09 17:03:10 +0200

    add: user id

--- models/user_tag.go
+++ models/user_tag.go
@@ -2,6 +2,7 @@ package models
 
 type UserTag struct {
 	Username  string `json:"username" bson:"username"`
+	UserId    string `json:"user_id" bson:"user_id"`
 	IdTag     string `json:"id_tag" bson:"id_tag"`
 	IsEnabled bool   `json:"is_enabled" bson:"is_enabled"`
 	Note      string `json:"note" bson:"note"`
