002713b99fdeb4bb34b73eef2ce2b33a8f6f4fd8
programador <rkhut@h2plt.energy>
2023-07-19 13:02:31 +0200

    add: price per hour

--- billing/affleck.go
+++ billing/affleck.go
@@ -24,32 +24,48 @@ func (a *Affleck) SetLogger(logger internal.LogHandler) {
 
 func (a *Affleck) OnTransactionFinished(transaction *models.Transaction) error {
 
+	// price in cents per hour
+	pricePerHour := 100
 	// price in cents per kW
 	pricePerKw := 30
+
+	// consumed minutes, minus 1 hour for the first hour
+	duration := transaction.TimeStop.Sub(transaction.TimeStart).Minutes() - 60
 	// consumed Watts
 	consumed := transaction.MeterStop - transaction.MeterStart
 
+	price := 0
+	if duration > 0 {
+		price = int(duration) * pricePerHour / 60
+	}
 	if consumed > 0 {
-		// total price in cents
-		price := consumed * pricePerKw / 1000
-		transaction.PaymentAmount = price
+		price = price + consumed*pricePerKw/1000
 	}
 
+	transaction.PaymentAmount = price
 	return nil
 }
 
 func (a *Affleck) OnMeterValue(transaction *models.Transaction, meterValue *models.TransactionMeter) error {
 
+	// price in cents per hour
+	pricePerHour := 100
 	// price in cents per kW
 	pricePerKw := 30
+
+	// consumed minutes, minus 1 hour for the first hour
+	duration := meterValue.Time.Sub(transaction.TimeStart).Minutes() - 60
 	// consumed Watts
 	consumed := meterValue.Value - transaction.MeterStart
 
+	price := 0
+	if duration > 0 {
+		price = int(duration) * pricePerHour / 60
+	}
 	if consumed > 0 {
-		// total price in cents
-		price := consumed * pricePerKw / 1000
-		meterValue.Price = price
+		price = price + consumed*pricePerKw/1000
 	}
 
+	meterValue.Price = price
 	return nil
 }
