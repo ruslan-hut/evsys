982c11957ce52f7228dd4ea8c448f17739ea0862
ruslanhut <dev@programmer.com.ua>
2024-01-11 21:01:58 +0100

    disabled meter values logging

--- server/system_handler.go
+++ server/system_handler.go
@@ -579,8 +579,8 @@ func (h *SystemHandler) OnMeterValues(chargePointId string, request *core.MeterV
 			}
 		}
 
-	} else {
-		h.logger.FeatureEvent(request.GetFeatureName(), chargePointId, fmt.Sprintf("%v", request.MeterValue))
+		//} else {
+		//	h.logger.FeatureEvent(request.GetFeatureName(), chargePointId, fmt.Sprintf("%v", request.MeterValue))
 	}
 	return core.NewMeterValuesResponse(), nil
 }
