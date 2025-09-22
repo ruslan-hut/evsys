0cda3558bd8556c86df0425873f304ee549a7277
programador <rkhut@h2plt.energy>
2025-09-22 14:07:21 +0200

    feat: add description field to PaymentPlan entity

--- entity/payment_plan.go
+++ entity/payment_plan.go
@@ -4,6 +4,7 @@ import "time"
 
 type PaymentPlan struct {
 	PlanId       string `json:"plan_id" bson:"plan_id"`
+	Description  string `json:"description" bson:"description"`
 	IsDefault    bool   `json:"is_default" bson:"is_default"` // global default, for all users
 	IsActive     bool   `json:"is_active" bson:"is_active"`
 	PricePerKwh  int    `json:"price_per_kwh" bson:"price_per_kwh"`
